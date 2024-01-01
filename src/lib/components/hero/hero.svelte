cad81676c5668e187c015191b86adffb86ac9fd2
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-01 12:46:19 -0800

    Hero Component Resolves #6

--- src/lib/components/hero/hero.svelte
+++ src/lib/components/hero/hero.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import { Particles } from "../particles";
-
 </script>
 <div>
-    <Particles/>
-    <h1>Hero</h1>
+    <span>Hi, My name is</span>
+    <h1 class='text-6xl'>Xavier Loera Flores</h1>
+    <h2 class='text-6xl'>I'm a Software Developer</h2>
+    <p class='text-2xl'>I am a diligent software engineer with 2+ years of experience in automation and software development. Eager to join any team seeking to build innovative and cutting-edge business solutions for users worldwide.</p>
 </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang ="ts">
+    import {Hero} from "$lib"
+
+</script>
+
+<div>
+    <Hero/>
+</div>
