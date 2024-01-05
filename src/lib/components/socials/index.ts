1ba69c3057d0c36334f3879c7fd84f151c568e0d
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-04 16:50:41 -0800

    Socials Resolves #21

---
+++ src/lib/components/socials/index.ts
@@ -0,0 +1,6 @@
+import GitHubSocial from './github.svelte';
+import TwitterSocial from './twitter.svelte';
+import LinkedInSocial from './linkedin.svelte';
+import SiteSocial from './site.svelte';
+
+export { GitHubSocial, TwitterSocial, LinkedInSocial, SiteSocial };
