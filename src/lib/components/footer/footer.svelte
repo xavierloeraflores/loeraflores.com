f883850a7d15b03493eaf5628cfb4f31fa4abed3
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-07 22:51:35 -0800

    Added cn twmerge to Footer Component Resolves #54

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -1,4 +1,14 @@
-<footer class="flex flex-col items-center justify-center divide-border border-t">
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from "$lib/utils";
+  type $$Props = HTMLAttributes<HTMLDivElement>;
+  let className: $$Props["class"] = undefined;
+  export { className as class };
+</script>
+
+<footer class= {cn("flex flex-col items-center justify-center divide-border border-t",
+  className
+)}>
     <a href="/">
       <span class="font-bold text-primary-foreground">Xavier Loera Flores</span>
     </a>
