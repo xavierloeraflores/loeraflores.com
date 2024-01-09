ccc119c542e39c57ca67cd515a807baa7b442dd8
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-09 00:14:07 -0800

    Typewriter Hero Component Resolves #58

--- src/lib/components/hero/hero.svelte
+++ src/lib/components/hero/hero.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import Typewriter from 'svelte-typewriter'
     import { Button, buttonVariants } from "$lib";
     import {toast} from 'svelte-sonner'
     import type { HTMLAttributes } from "svelte/elements";
@@ -17,11 +18,11 @@
 </script>
 
 <div class ={cn(
-    "p-4",
+    "p-4 h-screen",
     className
 )}>
     <span class='text-primary scroll-m-20  pb-2 text-2xl font-semibold tracking-tight first:mt-0'>Hi, my name is</span>
-    <h1 class=' text-primary-foreground scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Xavier Loera Flores.</h1>
+    <h1 class=' text-primary-foreground scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'><Typewriter>Xavier Loera Flores.</Typewriter></h1>
     <h2 class='text-accent-foreground scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0'>I'm a Software Developer</h2>
     <p class='text-2xl text-muted-foreground  w-2/3 mt-4 max-w-[750px] leading-7 [&:not(:first-child)]:mt-6'>I am a diligent software engineer with 2+ years of experience in automation and software development. Eager to join any team seeking to build innovative and cutting-edge business solutions for users worldwide.</p>
     <div class='flex flex-row mt-4'>
