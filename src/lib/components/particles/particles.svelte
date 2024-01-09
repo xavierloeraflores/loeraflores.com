72acc61df091b8f8bd51700cd037872f7d6b2052
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-09 15:57:45 -0800

    Removed Particles Background

--- src/lib/components/particles/particles.svelte
+++ src/lib/components/particles/particles.svelte
@@ -18,18 +18,25 @@
 		// you can use onMount to trigger the start of the animation
 		// or any other function
 	});
+// Navy				#0a192f
+// Light Navy		#112240 
+// Lightest Navy	#233554 
+// Slate			#8892b0 
+// Light Slate		#a8b2d1 
+// Lightest Slate	#ccd6f6 
+// White			#e6f1ff 
+// Green 			#64ffda
 
 	let particlesConfig ={
-	background: {
-		// color: "#0b032d"
-		color: "#233554"
-	},
+	// background: {
+	// 	color: "#233554"
+	// },
 	backgroundMode: {
 		enable: true
 	},
 	particles: {
 		color: {
-		value: ["#0a192f", "#112240", "#8892b0"]
+		value: ["#0a192f", "#112240", "#8892b0", "#8892b0"]
 		},
 		links: {
 		color: "#ccd6f6",
