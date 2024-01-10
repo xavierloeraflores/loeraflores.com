c404f2b2dc83f1c6aa11675d50454ee7d722e5fe
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 08:56:36 -0800

    Added More Projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -52,5 +52,161 @@ const Kurios: Project = {
 	],
 	technologies: ['React', 'Node', 'Express', 'PostgreSQL', 'Heroku']
 };
+const Snake: Project = {
+	title: 'Snake',
+	description: 'Classic Snake Game with vanilla JS',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/Arcade',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Live',
+			url: 'https://xavierloeraflores.github.io/Arcade/',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['HTML', 'CSS', 'JavaScript']
+};
+const GitHubio: Project = {
+	title: 'GitHub.io URL Converter',
+	description: 'Get the GitHub repo URL from a GitHub.io URL',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/github-url-converter',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Live',
+			url: 'https://github-url-converter.vercel.app/',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Vercel']
+};
+const SchedulingManager: Project = {
+	title: 'Java Scheduling Manager GUI',
+	description: 'A Java GUI for managing appointments',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/Scheduling-Manager-GUI',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Docs',
+			url: 'https://xavierloeraflores.github.io/Scheduling-Manager-GUI/',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['Java', 'JavaFX', 'MySQL', 'Time/Language Localization', 'Java Doc']
+};
+const InventoryManager: Project = {
+	title: 'Java Inventory Manager GUI',
+	description: 'A Java GUI for managing Inventory',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/Inventory-Management-GUI',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Docs',
+			url: 'https://xavierloeraflores.github.io/Inventory-Management-GUI/',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['Java', 'JavaFX', 'Java Doc']
+};
+const PandoraBot: Project = {
+	title: 'Careers Chatbot',
+	description: 'A simple chatbot for computer science careers',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/Pandorabot-Chatbot',
+			icon: ProjectLinkIcon.GitHub
+		}
+	],
+	technologies: ['AIML', 'PandoraBots']
+};
+const IOSTips: Project = {
+	title: 'IOS Tipping App',
+	description: 'A simple tipping app for IOS',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com:xavierloeraflores/tipIOSApp',
+			icon: ProjectLinkIcon.GitHub
+		}
+	],
+	technologies: ['IOS', 'Swift', 'Swift UI', 'XCode']
+};
+const Flixster: Project = {
+	title: 'Flixster Web App',
+	description: 'A website for viewing trailers and movie information',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/Flixster-Web-App',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Live',
+			url: 'https://xavierloeraflores.github.io/Flixster-Web-App/',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['HTML', 'CSS', 'JavaScript']
+};
+const Synema: Project = {
+	title: 'Synema (In Progress)',
+	description: 'A website for viewing trailers and movie information',
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/synema',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Live',
+			url: 'https://synema.vercel.app/',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Vercel']
+};
+const Xavierslist: Project = {
+	title: "Xavier's List (In Progress)",
+	description: "Craig's List clone",
+	links: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/xavierloeraflores/xavierslist',
+			icon: ProjectLinkIcon.GitHub
+		},
+		{
+			name: 'Live',
+			url: 'https://xavierslist.vercel.app',
+			icon: ProjectLinkIcon.Live
+		}
+	],
+	technologies: ['Go', 'Fiber', 'Next.js', 'TypeScript', 'Tailwind CSS', 'Vercel']
+};
 
-export const projectsData: Project[] = [Notes, MappyCoin, Kurios];
+export const projectsData: Project[] = [
+	Xavierslist,
+	Synema,
+	Notes,
+	MappyCoin,
+	Kurios,
+	Snake,
+	GitHubio,
+	SchedulingManager,
+	InventoryManager,
+	PandoraBot,
+	IOSTips,
+	Flixster
+];
