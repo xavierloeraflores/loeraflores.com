f4d097aab7cdba53ee8bfe78c2375d4ef6425dd7
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 07:17:36 -0800

    Education-Card Component Closes #12

---
+++ src/lib/components/education/education-card.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { type Education } from '../../../types';
+    import {toast} from 'svelte-sonner'
+    import {Card, CardContent, CardTitle, CardDescription, CardFooter, CardHeader} from "$lib/components/ui/card"
+    import {GraduationCap } from 'lucide-svelte'
+    import { Separator} from '$lib/components/ui/separator'
+
+    export let edu: Education;
+</script>
+
+<Card class="bg-background">
+    <CardHeader class="flex justify-items-center justify-center align-middle items-center">
+        <CardTitle class = "text-primary scroll-m-20  pb-2 text-2xl font-semibold tracking-tight first:mt-0">{edu.degree}</CardTitle>
+        <CardDescription class="flex flex-row items-center justify-center ">
+            <span class="text-lg font-medium leading-none mr-2">{edu.dates}</span>
+        </CardDescription>
+    </CardHeader>
+    <CardContent class="flex justify-center items-centers justify-items-center">
+        <!-- <p class="text-lg font-medium leading-none mr-2">{edu.school}</p> -->
+    </CardContent>
+    <Separator />
+    <CardFooter class="flex flex-row p-4 w-full justify-evenly">
+        <a href="https://www.wgu.edu/" class="text-lg font-medium leading-none mr-2">{edu.school}, {edu.location}</a>
+    </CardFooter>
+</Card>
--- src/lib/components/education/education.svelte
+++ src/lib/components/education/education.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import EducationCard from "$lib/components/education/education-card.svelte"
     import { type Education } from '../../../types';
     import { Section, Button } from '$lib';
     import {toast} from 'svelte-sonner'
@@ -14,28 +15,14 @@
 <Section title="Education" class="flex-col justify-center content-center align-middle items-center">
     <GraduationCap size="64" class="color-primary-foreground"/>
     <Tabs class='max-w-[750px]'>
-        <List class="grid w-full grid-cols-5">
+        <List class={`grid w-full grid-cols-${degreeCount}`}>
             {#each educationData as edu}
             <Trigger value={edu.degree}>{edu.tab}</Trigger>
             {/each}
         </List>
         {#each educationData as edu}
         <Content value ={edu.degree}>
-            <Card class="bg-background">
-                <CardHeader class="flex justify-items-center justify-center align-middle items-center">
-                    <CardTitle class = "text-primary scroll-m-20  pb-2 text-2xl font-semibold tracking-tight first:mt-0">{edu.degree}</CardTitle>
-                    <CardDescription class="flex flex-row items-center justify-center ">
-                        <span class="text-lg font-medium leading-none mr-2">{edu.dates}</span>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent class="flex justify-center items-centers justify-items-center">
-                    <!-- <p class="text-lg font-medium leading-none mr-2">{edu.school}</p> -->
-                </CardContent>
-                <Separator />
-                <CardFooter class="flex flex-row p-4 w-full justify-evenly">
-                    <a href="https://www.wgu.edu/" class="text-lg font-medium leading-none mr-2">{edu.school}, {edu.location}</a>
-                </CardFooter>
-            </Card>
+            <EducationCard edu={edu}/>
         </Content>
         {/each}
     </Tabs>
