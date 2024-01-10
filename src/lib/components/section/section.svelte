ac7dec988f77a513c5f1b3a0e01622f89bb539dd
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 09:29:43 -0800

    Section Component Styling

--- src/lib/components/section/section.svelte
+++ src/lib/components/section/section.svelte
@@ -11,13 +11,13 @@
     export let title: string;
 </script>
 
-<Saos animation={'fade-in 2.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'} top={250} bottom={250} once={true} >
+<Saos animation={'fade-in 2.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'} top={250} bottom={250} once={true}>
 <section id={title} class={cn(
-    "w-full h-screen flex flex-col items-center",
+    "w-screen min-h-96 flex flex-col items-center",
     className
     )}>
-    <h2 class="w-full text-center text-2xl text-muted-foreground scroll-m-20 border-b pb-2 font-semibold tracking-tight transition-colors first:mt-0">{title}</h2>
-    <Separator class="w-full"/>
-        <slot />
-    </section>
-</Saos>
+            <h2 class="w-full text-center text-4xl sm:text-2xl text-primary scroll-m-20 pb-2 font-semibold tracking-tight transition-colors first:mt-0">{title}</h2>
+            <Separator class=" w-11/12 my-2"/>
+                <slot class=" max-w-screen-2xl"/>
+        </section>
+    </Saos>
