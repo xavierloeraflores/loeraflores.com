4fc8d144e640026a5820b9142d3fdc832fb1860f
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 09:04:27 -0800

    Added a grid to the Portfolio Section

--- src/lib/components/portfolio/portfolio.svelte
+++ src/lib/components/portfolio/portfolio.svelte
@@ -5,9 +5,10 @@ export let projects: ProjectType[];
 
 </script>
 
-<Section title="Portfolio">
-    {#each projects as project}
+<Section title="Portfolio" class="">
+    <div class="grid gap-8 grid-flow-row xl:grid-cols-4 lg:grid-cols-3 md: grid-cols-2 sm:grid-cols-1">
+        {#each projects as project}
         <Project {project}/>
-    {/each}
-
+        {/each}
+    </div>
 </Section>
