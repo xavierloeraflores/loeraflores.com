f3207b6e322358de0dd6c1f6a69e73d9eacdd87a
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 07:24:03 -0800

    Lightswitch Styling Closes #65

--- src/lib/components/lightswitch/lightswitch.svelte
+++ src/lib/components/lightswitch/lightswitch.svelte
@@ -4,9 +4,9 @@
     import { toggleMode } from "mode-watcher";
   </script>
   
-  <Button on:click={toggleMode}  size="icon" class="bg-transparent divide-primary">
+  <Button on:click={toggleMode}  size="icon" class="bg-transparent divide-primary text-primary pt-3">
     <Sun
-      class="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      class="absolute h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
     />
     <Moon
       class="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
