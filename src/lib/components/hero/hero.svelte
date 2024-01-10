2cb88009271bf2d4284fc0ddf62d89e4e1159096
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 11:26:28 -0800

    Removed Max-w on sm screens Closes #69

--- src/lib/components/hero/hero.svelte
+++ src/lib/components/hero/hero.svelte
@@ -25,7 +25,7 @@
     <span class='text-primary scroll-m-20  pb-2 text-2xl font-semibold tracking-tight first:mt-0 lg:text-4xl'>Hi, my name is</span>
     <h1 class=' text-secondary-foreground scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl'><Typewriter>Xavier Loera Flores.</Typewriter></h1>
     <h2 class='text-accent-foreground scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 lg:text-5xl'>I'm a Software Developer</h2>
-    <p class='text-2xl text-muted-foreground  w-2/3 mt-4 max-w-[750px] leading-7 [&:not(:first-child)]:mt-6 lg:max-w-[1200px] lg:text-3xl text-center'>I am a diligent software engineer with 2+ years of experience in automation and software development. Eager to join any team seeking to build innovative and cutting-edge business solutions for users worldwide.</p>
+    <p class='text-2xl text-muted-foreground sm:w-11/12  w-2/3 mt-4 max-w-[750px] leading-7 [&:not(:first-child)]:mt-6 lg:max-w-[1200px] lg:text-3xl text-center'>I am a diligent software engineer with 2+ years of experience in automation and software development. Eager to join any team seeking to build innovative and cutting-edge business solutions for users worldwide.</p>
     <div class='flex flex-row mt-4'>
         <Button on:click={calendly} class='mx-4'>Get in touch</Button>
         <a href="/files/resume.pdf" download on:click={resume} class={buttonVariants({variant:"default", size:"default"})}>Resume</a>
