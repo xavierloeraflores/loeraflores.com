d8ad0d3888ff555723ad0cf88b02c5f554f77497
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-01-10 11:57:25 -0800

    Portfolio Icon closes #72

--- src/lib/components/portfolio/portfolio.svelte
+++ src/lib/components/portfolio/portfolio.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import {FolderGit2} from 'lucide-svelte'
     import Project from "./project.svelte";
     import { Section } from '$lib';
     import type { Project as ProjectType } from '../../../types';
@@ -6,6 +7,7 @@
 </script>
 
 <Section title="Portfolio" class="">
+    <FolderGit2 size="64" class="color-primary-foreground my-4"/>
     <div class="grid gap-8 grid-flow-row xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1">
         {#each projects as project}
         <Project {project}/>
