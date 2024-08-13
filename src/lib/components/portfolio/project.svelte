ea602c03661d4deb52fd33b4af33adb263f7356c
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-08-12 23:01:44 -0700

    updated project car styling to add shadows

--- src/lib/components/portfolio/project.svelte
+++ src/lib/components/portfolio/project.svelte
@@ -1,42 +1,51 @@
 <script lang="ts">
-    import {Card, CardContent, CardTitle, CardDescription, CardFooter, CardHeader} from "$lib/components/ui/card"
-    import { Button } from "$lib/components/ui/button";
-    import Badge from "../ui/badge/badge.svelte";
-    import type { HTMLAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils";
-	import type { Project } from "../../../types";
-	import type { ProjectLink } from "../../../types/project";
+	import {
+		Card,
+		CardContent,
+		CardTitle,
+		CardDescription,
+		CardFooter,
+		CardHeader
+	} from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+	import Badge from '../ui/badge/badge.svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils';
+	import type { Project } from '../../../types';
+	import type { ProjectLink } from '../../../types/project';
 
-    type $$Props = HTMLAttributes<HTMLDivElement> & {
-        project:Project
-    };
-	let className: $$Props["class"] = undefined;
+	type $$Props = HTMLAttributes<HTMLDivElement> & {
+		project: Project;
+	};
+	let className: $$Props['class'] = undefined;
 	export { className as class };
-    export let project:Project
+	export let project: Project;
 
-    function linkButton(link:ProjectLink) {
-        window.open(link.url, '_blank')
-    }
-
+	function linkButton(link: ProjectLink) {
+		window.open(link.url, '_blank');
+	}
 </script>
 
-<Card class={cn(
-    "rounded-lg w-96 border bg-card text-card-foreground shadow flex flex-col justify-between",
-    className
-)}>
-    <CardHeader class=' min-h-28'>
-        <CardTitle>{project.title}</CardTitle>
-        <CardDescription>{project.description}</CardDescription>
-    </CardHeader>
-    <CardContent>
-        {#each project.technologies as tag}
-        <Badge class="m-1">{tag}</Badge>
-        {/each}
-    </CardContent>
-    <CardFooter class="flex flex-row items-center justify-evenly">
-        {#each project.links as link}
-        <Button variant="secondary" on:click={()=>linkButton(link)}>{link.name}</Button>
-        {/each}
-    </CardFooter>
-
+<Card
+	class={cn(
+		'flex w-96 flex-col justify-between rounded-lg border bg-card text-card-foreground shadow-lg',
+		className
+	)}
+>
+	<CardHeader class=" min-h-28">
+		<CardTitle>{project.title}</CardTitle>
+		<CardDescription>{project.description}</CardDescription>
+	</CardHeader>
+	<CardContent>
+		{#each project.technologies as tag}
+			<Badge class="m-1">{tag}</Badge>
+		{/each}
+	</CardContent>
+	<CardFooter class="flex flex-row items-center justify-evenly">
+		{#each project.links as link}
+			<Button variant="secondary" class="shadow-md" on:click={() => linkButton(link)}
+				>{link.name}</Button
+			>
+		{/each}
+	</CardFooter>
 </Card>
