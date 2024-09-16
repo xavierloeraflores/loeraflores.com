53a98cd70733a743ffefea552df9d9bfff1c97dc
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-09-16 00:35:44 -0700

    edu card styling

--- src/lib/components/education/education-card.svelte
+++ src/lib/components/education/education-card.svelte
@@ -9,28 +9,32 @@
 <Card
 	class="m-2 flex h-40 flex-row items-center bg-background shadow-md shadow-primary lg:shadow-lg lg:shadow-primary"
 >
-	<div id="image" class="mr-8 sm:ml-2 md:ml-8">
+	<div id="image" class="ml-2 mr-8 md:ml-8">
 		{#if edu.imageUrl}
-			<img src={edu.imageUrl} alt={edu.school} class="h-24 w-24 object-contain" />
+			<img
+				src={edu.imageUrl}
+				alt={edu.school}
+				class="h-16 w-16 object-contain sm:h-20 sm:w-20 md:h-24 md:w-24"
+			/>
 		{:else}
-			<GraduationCap class="h-24 w-24 text-primary" />
+			<GraduationCap class="h-16 w-16 text-primary sm:h-20 sm:w-20 md:h-24 md:w-24" />
 		{/if}
 	</div>
 
 	<CardContent class="flex flex-col items-center justify-center justify-items-center">
 		<div class="mt-2 flex flex-col justify-start">
 			<CardTitle
-				class="scroll-m-20 text-2xl font-semibold tracking-tight text-primary first:mt-0 sm:text-3xl"
+				class="scroll-m-20 text-xl font-semibold tracking-tight text-primary sm:text-2xl md:pt-1 lg:text-3xl"
 			>
 				{edu.degree}
 			</CardTitle>
 
 			<CardDescription class="items-left justify-left mb-1 flex flex-col">
-				<span class="text-md mr-2 font-medium leading-none sm:text-lg">{edu.dates}</span>
+				<span class="text-md mr-2 font-medium leading-none md:text-lg">{edu.dates}</span>
 			</CardDescription>
 			<a
 				href={edu.link}
-				class="text-md font-medium leading-none hover:underline sm:text-lg"
+				class="text-md font-medium leading-none hover:underline md:text-lg"
 				target="_blank">{edu.school}<span class="hidden md:inline">, {edu.location}</span></a
 			>
 		</div>
