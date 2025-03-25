a3d35220ac1b37215162636f0f26c212d56c9613
xavierloeraflores <x.loera.tech@gmail.com>
2025-03-25 16:19:56 -0700

    Added socials to footer

--- site/src/components/footer.tsx
+++ site/src/components/footer.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="bg-background z-10 flex h-24 w-full flex-col gap-4 pt-4">
+      <div
+        id="footer-socials"
+        className="flex items-center justify-center gap-4"
+      >
+        <Link href="https://github.com/xavierloeraflores">
+          <FaGithub className="h-6 w-6" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/xavierloeraflores/">
+          <FaLinkedin className="h-6 w-6" />
+        </Link>
+        <Link href="https://x.com/TheLoeraFlores ">
+          <FaXTwitter className="h-6 w-6" />
+        </Link>
+      </div>
+
+      <p className="text-muted-foreground w-full text-center text-sm">
+        &copy; {2025} Xavier Loera Flores. All rights reserved.
+      </p>
     </footer>
   );
 }
