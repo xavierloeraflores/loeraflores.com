c7bfffdd4c71ba8613c65d32566cfb619196ef2e
Xavier Loera Flores <x.loera.tech@gmail.com>
2025-03-29 03:32:50 -0700

    Update Home page layout to increase gap between components

--- site/src/app/(pages)/page.tsx
+++ site/src/app/(pages)/page.tsx
@@ -3,7 +3,7 @@ import Testimonials from "~/components/testimonials";
 import Contact from "~/components/contact";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-4">
+    <main className="flex min-h-screen flex-col gap-8">
       <Hero />
       <Testimonials />
       <Contact />
