a597931dd7e709f9226ee45fa54935acc2507d0a
xavierloeraflores <x.loera.tech@gmail.com>
2025-04-09 07:49:30 -0700

    Added spacing to project grid

--- site/src/components/projects.tsx
+++ site/src/components/projects.tsx
@@ -15,7 +15,7 @@ export default function Projects() {
   return (
     <section className="flex flex-col items-center gap-4">
       <h2 className="text-2xl font-bold">Projects</h2>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 xl:gap-6">
         {projects.map((project) => (
           <ProjectCard key={project.title} project={project} />
         ))}
